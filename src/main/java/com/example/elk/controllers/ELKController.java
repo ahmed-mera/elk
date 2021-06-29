c523791fe00a3784a68be1e3baa60dc088942366
Ahmed mera <ahmed.mera@piksel.com>
2021-06-29 13:36:32 +0200

    fix

--- src/main/java/com/example/elk/controllers/ELKController.java
+++ src/main/java/com/example/elk/controllers/ELKController.java
@@ -1,27 +1,14 @@
 package com.example.elk.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.Date;
 
 @RestController
 @Slf4j
 public class ELKController {
-
-    @Autowired
-    RestTemplate restTemplate;
-
-    @Bean
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     @RequestMapping(value = "/elk")
     public String helloWorld() {
@@ -37,17 +24,9 @@ public class ELKController {
         try {
             throw new Exception("Exception has occurred....");
         } catch (Exception e) {
-            e.printStackTrace();
             log.error("error: {}", e.getMessage());
-
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            e.printStackTrace(pw);
-            String stackTrace = sw.toString();
-            log.error("Exception - " + stackTrace);
-            response = stackTrace;
+            log.error("Exception - " , e);
         }
-
         return response;
     }
 }
